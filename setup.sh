4365d8bd5aa5f98b90d90b1d840d8c4b3ce5a6a8
Sindre S Brurberg <sindrsb@student.matnat.uio.no>
2018-01-16 11:49:59 +0100

    Added uppdating of gits

--- setup.sh
+++ setup.sh
@@ -1,28 +1,59 @@
-function clone {
-    if [ ! -d "SPI-Py" ]; then
+function install {
+    python setup.py install
+    cd ..
+}
+function pullret {
+    git pull
+    cd ..
+}
+
+function uppdate {
+    git pull > $pullMsg
+    if [ ! $pullMsg = *"Already up tp date."*]; then
+        exec setup.sh
+        exit
+    fi; if [ ! -d "SPI-Py" ]; then
         git clone https://github.com/lthiery/SPI-Py.git
         cd SPI-Py
-        python setup.py setup
-        cd ..
+        install
+    else
+        cd SPI-Py
+        git pull
+        install
     fi; if [ ! -d "MFRC522-python" ]; then
         git clone https://github.com/mxgxw/MFRC522-python.git
+    else
+        cd MFRC522-python
+        pullret
     fi;
     if [ "$HOSTNAME" = *"door"* ]; then
-        if [ ! -d "nfc_door" ]; then
-            git clone https://github.com/InfectedLan/nfc_door.git
+        $door = "nfc_door"
+        if [ ! -d $door ]; then
+            git clone https://github.com/InfectedLan/$door.git
+        else
+            cd $door
+            pullret
         fi
     fi; if [ "$HOSTNAME" = *"kafe"* ]; then
-        if [ ! -d "kafe_client_nfc" ]; then
-            git clone https://github.com/InfectedLan/kafe_client_nfc.git
+        $kafe = "kafe_client_nfc"
+        if [ ! -d $kafe ]; then
+            git clone https://github.com/InfectedLan/$kafe.git
+        else
+            cd $kafe
+            pullret
         fi
     fi; if [ "$HOSTNAME" = *"checkin"* ]; then
-        if [ ! -d "nfc_checkin" ]; then
-            git clone https://github.com/InfectedLan/nfc_checkin.git
+        $checkin = "nfc_checkin"
+        if [ ! -d $checkin ]; then
+            git clone https://github.com/InfectedLan/$checkin.git
+        else
+            cd $checkin
+            pullret
         fi
     fi
 }
 
 function init {
-    clone
+    uppdate
     cd ~/
 }
