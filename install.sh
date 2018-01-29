0ed8f5eba8c4d559f4f54788f77f2fcc73a44c07
Sindre S Brurberg <sindrsb@student.matnat.uio.no>
2018-01-29 14:42:27 +0100

    Fixed sd card settup and started install script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-exec pacman -Syu --noconfirm
-exec pacman -S vim tmux --noconfirm
+ssh alarm@NFC-Client-setup
+su
+pacman -Syu --noconfirm
+pacman -S vim tmux --noconfirm
 "en_US.UTF-8 UTF-8" > /etc/locale.gen
-exec locale-gen
+locale-gen
 "LANG=en_US.UTF-8" > /etc/locale.conf
-exec reboot
+reboot
