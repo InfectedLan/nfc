32c97ea0280ab726d202c39a37d94e8ea6b816a2
Sindre S Brurberg <sindrsb@student.matnat.uio.no>
2018-02-11 12:55:14 +0100

    qrscanner

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 ssh alarm@NFC-Client-setup
 su
 pacman -Syu --noconfirm
-pacman -S vim tmux python python-numpy opencv zbar hdf5 pip --noconfirm
+pacman -S vim tmux python python-numpy opencv zbar hdf5 pip git --noconfirm
 pip install zbar-py
 "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
