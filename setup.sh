5ddefc257e4b4f6d251b0906a65cced2cdfebe61
Sindre S Brurberg <sindrsb@student.matnat.uio.no>
2018-02-13 22:13:49 +0100

    Fixed auto start

--- setup.sh
+++ setup.sh
@@ -23,16 +23,17 @@ function uppdate {
         cd MFRC522-python
         git pull
         cd ..
+        cp ./MFRC522-python/MFRC522.py ./src/MFRC522.py
     fi;
 }
 
 function run {
     uppdate
-    if [ "$HOSTNAME" = *"door"* ]; then
+    if [[ "$HOSTNAME" = *"door"* ]]; then
         python ./src/door.py
-    fi; if [ "$HOSTNAME" = *"kafe"* ]; then
-        python ./kafe.py
-    fi; if [ "$HOSTNAME" = *"checkin"* ]; then
+    fi; if [[ "$HOSTNAME" = *"kafe"* ]]; then
+        python ./src/kafe.py
+    fi; if [[ "$HOSTNAME" = *"checkin"* ]]; then
         python ./src/checkin.py
     fi
 }
